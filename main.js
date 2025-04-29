50c2fd183c70371d247610030e3201c2e979bb91
RithekShankar <145854041+RithekShankar@users.noreply.github.com>
2025-04-29 17:50:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshDepthMaterial( { wireframe: true } );
+const material = new THREE.MeshDepthMaterial( { wireframe: true ,blendColor: 0xffffff} );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
